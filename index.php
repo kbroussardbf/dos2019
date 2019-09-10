7b969bd2629cb52ebc3228e961625612b513cd87
kbroussardbf <45106009+kbroussardbf@users.noreply.github.com>
2019-09-10 09:14:12 -0400

    Update index.php

--- index.php
+++ index.php
@@ -12,13 +12,13 @@
 <div id="right" align="right" style="display:inline-block">
 <h1>Welcome<?php if ($USER) { echo " ".$USER; } else { echo " Intranet User"; } ?>!</h1>
 <ul>
-<li><a href="reset.php">Reset your password</a>
+<li><a href="<!--reset.php->">Reset your password</a>
 </li>
 <li>
-<a href="directory.php">Shared directory listing</a>
+<a href="<!--directory.php-->">Shared directory listing</a>
 </li>
 <li>
-<a href="updateTable.php">Data Management Interface (DMI)</a>
+<a href="<!--updateTable.php-->">Data Management Interface (DMI)</a>
 </li>
 </ul>
 </div>
