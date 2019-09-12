5c42a08f1e0e60c0cc414332e5a15670c07894e8
kbroussardbf <45106009+kbroussardbf@users.noreply.github.com>
2019-09-11 22:00:56 -0400

    Update reset3.php

--- reset3.php
+++ reset3.php
@@ -2,7 +2,32 @@
 //$USER = $_GET['user'];
 $email = $_COOKIE['email'];
 
+$servername = "localhost";
+$dbname = "dos2019yyz";
+$username = "test";
+$password = "pass";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$update = "UPDATE Users SET pwd='".$_POST['newPassword']."' WHERE email='".$email."'";
+
+if (isset($_POST['submit'])) {
 //action: update password for $email to $_POST['newPassword'];
+   if (mysqli_query($conn, $update)) {
+      //redirect to index.php
+      header('Location: index.php'); 
+   } else {
+      echo "Error updating record: " . mysqli_error($conn);
+   }
+}
+
+mysqli_close($conn);
 ?>
 
 <html>
@@ -11,9 +36,10 @@ $email = $_COOKIE['email'];
 <body>
 <h1>Reset Password</h1>
 <p>
-form action="reset3.php"<br/>
-New password:<input id="newPassword" name="newPassword" value="" /><br/>
-Reset button
+<form action="reset3.php" method="POST" />
+New password:<br/>
+<input type="text" id="newPassword" name="newPassword" value="" /> <input type="submit" id="submit" name="submit" value="Reset Password" />
+</form>
 </p>
 </div>
 </body>
