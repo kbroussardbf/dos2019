9eb40eded212bfe9dd524979d5e21b2ea85690ec
kbroussardbf <45106009+kbroussardbf@users.noreply.github.com>
2019-09-11 22:05:27 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,26 +1,15 @@
-<?php
-//$USER = $_GET['user'];
-?>
-
 <html>
 <head>
 </head>
 <body>
 <div id="left" style="display:inline-block">
-<img src="media/Beagle-Care.jpg" alt="Intranet logo" height="200px" width="" />
 </div>
 <div id="right" align="right" style="display:inline-block">
-<h1>Welcome<?php if ($USER) { echo " ".$USER; } else { echo " Intranet User"; } ?>!</h1>
-<ul>
-<li><a <!--href="reset.php"-->>Reset your password</a>
-</li>
-<li>
-<a <!--href="directory.php"-->>Shared directory listing</a>
-</li>
-<li>
-<a <!--href="updateTable.php"-->>Data Management Interface (DMI)</a>
-</li>
-</ul>
+<h1>Welcome<?php if ($USER) { echo " ".$USER; } else { echo " Workshop Participant"; } ?>!</h1>
+  <p>Turns out that Heroku is far too secure for me to deploy my super vulnerable app for the workshop (OWASP JS folks are magicians) <br/>
+  SO INSTEAD I've got a git project for you to download to /var/www/html and then just run ./var/www/html/dos2019yyz/install.sh and you should be all ready to go.<br/>
+  Hit me up with any questions and/or we'll get it all set up on Saturday.</p>
+  <p>DL THE APP: <a href="https://github.com/kbroussardbf/dos2019yyz/archive/master.zip" alt="master git repo for dos2019yyz">https://github.com/kbroussardbf/dos2019yyz/archive/master.zip</a></p>
 </div>
 </body>
 </html>
