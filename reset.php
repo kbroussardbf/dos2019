dee02c03af21807504f68a7e86c66d27b3c2d113
kbroussardbf <45106009+kbroussardbf@users.noreply.github.com>
2019-09-11 23:22:00 -0400

    Add files via upload

--- reset.php
+++ reset.php
@@ -2,10 +2,14 @@
 //$USER = $_GET['user'];
 $email = $_POST['email'];
 setrawcookie('email', $email, time() + (86400 * 30), "/");
+$uname = $_POST['uname'];
+setrawcookie('uname', $uname, time() + (86400 * 30), "/");
 
 //redirect to reset2.php
 if ($email) { 
     header('Location: reset2.php');
+} elseif ($uname) {
+    header('Location: setup.php');
 }
 ?>
 
@@ -18,7 +22,9 @@ if ($email) {
 
 <form method="POST" action="reset.php">
 <p>Reset password for email:<br/>
-<input type="email" value="" id="email" name="email"/>
+<input type="email" value="" id="email" name="email"/><br/>
+or for username:<br/>
+<input type="uname" value="" id="uname" name="uname"/>
 </p>
 <input type="submit" value="submit" id="submit" name="submit"/>
 </form>
---
+++ setup.php
@@ -0,0 +1,51 @@
+<?php
+$uname = $_COOKIE['uname'];
+
+$servername = "localhost";
+$dbname = "dos2019yyz";
+$username = "test";
+$password = "pass";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+//set question and answer for uname
+if (isset($_POST['submit'])) {
+        $insert = "UPDATE Users SET question='".$_POST['questn']."', answer='".$_POST['ans']."', email='".$_POST['email']."' WHERE uname='".$uname."'";
+
+        if (mysqli_query($conn, $insert)) {
+             //echo "Record inserted successfully";
+        } else {
+             echo "Error inserting record: " . mysqli_error($conn);
+        } 
+
+    //redirect to index.php?user=$_POST['user']
+    header("Location: index.php?user=".$uname."");
+}
+
+mysqli_close($conn);
+?>
+
+<html>
+<head>
+</head>
+<body>
+<div>
+<h1>Security Question</h1>
+<form action="setup.php" method="POST">
+Email:<br/>
+<input type="text" id="email" name="email" value=""/><br/>
+Question:<br/>
+<input type="text" id="questn" name="questn" value="What is your favorite snack food?" style="width:20em"/><br/>
+Answer:<br/>
+<input type="text" id="ans" name="ans" value=""/><br/>
+<input type="submit" id="submit" name="submit" value="Submit"/>
+</form>
+</div>
+</body>
+</html>
