cafc0475cb4f3711a9b7a8779707dd4df2773f5b
kbroussardbf <45106009+kbroussardbf@users.noreply.github.com>
2019-09-11 22:16:27 -0400

    Update index.php

--- index.php
+++ index.php
@@ -7,8 +7,10 @@
 <div id="right" align="right" style="display:inline-block">
 <h1>Welcome<?php if ($USER) { echo " ".$USER; } else { echo " Workshop Participant"; } ?>!</h1>
   <p>Turns out that Heroku is far too secure for me to deploy my super vulnerable app for the workshop (OWASP JS folks are magicians) <br/>
-  SO INSTEAD I've got a git project for you to download to /var/www/html and then just run ./var/www/html/dos2019yyz/install.sh and you should be all ready to go.<br/>
-  Hit me up with any questions and/or we'll get it all set up on Saturday.</p>
+  SO INSTEAD I've got a git project for you to download, <br/>
+    unzip into /var/www/html, <br/>
+    and then just run ./var/www/html/dos2019yyz/install.sh and you should be all ready to go.</p>
+  <p>Hit me up with any questions and/or we'll get it all set up on Saturday.</p>
   <p>DL THE APP: <a href="https://github.com/kbroussardbf/dos2019yyz/archive/master.zip" alt="master git repo for dos2019yyz">https://github.com/kbroussardbf/dos2019yyz/archive/master.zip</a></p>
 </div>
 </body>
