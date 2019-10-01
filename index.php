8ee956cbf27ccaa2d8a54118e11815ccd54123cb
kbroussardbf <45106009+kbroussardbf@users.noreply.github.com>
2019-10-01 15:43:18 -0400

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
   <p>Turns out that Heroku is far too secure for me to deploy my super vulnerable app for the workshop (OWASP JS folks are magicians) <br/>
     SO INSTEAD I've got a git project for you to download or git clone into your Kali VM: <br/> 
     cd /var/www/html && git clone https://github.com/kbroussardbf/dos2019.git<br/>
-    (unzip into /var/www/html and rename to dos2019 if needed), <br/>
+    (or unzip downloaded dir into /var/www/html and rename to dos2019 if needed), <br/>
     and then chmod +x /var/www/html/dos2019/install.sh<br/>
     and /var/www/html/dos2019/install.sh and you should be all ready to go!</p>
   <p>Your project will run locally at: <a href="http://localhost/dos2019/">http://localhost/dos2019/</a></p>
