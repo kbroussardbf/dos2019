e35c200d3e98c26a7105a5e9f5c79007eaa3c928
kbroussardbf <45106009+kbroussardbf@users.noreply.github.com>
2019-10-09 17:45:30 -0400

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
     and then chmod +x /var/www/html/dos2019/install.sh<br/>
     and /var/www/html/dos2019/install.sh and you should be all ready to go!</p>
   <p>Your project will run locally at: <a href="http://localhost/dos2019/">http://localhost/dos2019/</a></p>
-  <p>Hit me up with any questions and/or we'll get it all set up on Saturday.</p>
+  <p>Hit me up with any questions and/or we'll get it all set up on Friday.</p>
   <p>DL THE APP: <a href="https://github.com/kbroussardbf/dos2019/archive/master.zip" alt="master git repo for dos2019">https://github.com/kbroussardbf/dos2019/archive/master.zip</a></p>
   <p>GIT CLONE: cd /var/www/html && git clone https://github.com/kbroussardbf/dos2019.git
 </div>
